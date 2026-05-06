4743c4a864a117e417d9fb7692d007a3646536f2
p63207256-netizen <p63207256@gmail.com>
2026-05-06 17:02:12 -0300

    vesão atulizada

--- JAOAEMANUEL.py
+++ JAOAEMANUEL.py
@@ -1,3 +1,21 @@
 import streamlit as st
 st.title("Caculadora De Combustivel")
 st.subheader("Escolha Entre Gasolina Ou Etanol")
+gasosa = st.number_input("Digite O Valor Da Gasolina",min_value=0.0)
+etanol = st.number_input("Digite O Valor Da Etanol",min_value=0.0)
+
+if gasosa >0:
+
+    resultdo = etanol/gasosa
+    if resultdo <0.7:
+        mgs = "abastaça com Etanol"
+    else:
+        mgs = "abastaça com Gasosa"
+else:
+    st.warning("Os valores não podem ser 0") 
+
+
+if st.button("calcular"):
+     st.info(mgs)
+
+st.balloons
